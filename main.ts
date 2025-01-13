ce28ee4ea65d43e49e2e3e9a461847fac311bf1f
Shreyas Mididoddi <me@shreyascodes.tech>
2025-01-13 20:44:42 +0530

    update docs

--- main.ts
+++ main.ts
@@ -2,6 +2,24 @@ import { PurgeCSS, type Options, defaultOptions } from "purgecss";
 import type { Plugin } from "vite";
 import { defu } from "defu";
 
+/**
+ * A vite plugin for purgecss
+ * @param options - Options for purgecss
+ *
+ * @example
+ * ```ts
+ * import { defineConfig } from "vite";
+ * import purgecss from "@shreyas/vite-plugin-purgecss";
+ *
+ * export default defineConfig({
+ *   plugins: [
+ *     purgecss({
+ *	     	// ... options
+ *     }),
+ *   ],
+ * });
+ * ```
+ */
 export function purgecss(options: Partial<Options>): Plugin {
   const _options = defu(options, defaultOptions) as Options;
   return {
@@ -37,6 +55,28 @@ export function purgecss(options: Partial<Options>): Plugin {
 }
 
 const SVELTE_SAFELIST = /svelte-[a-zA-Z0-9]{6,8}/;
+/**
+ * A set of safelists provided for some popular frameworks
+ * only svelte is available for now
+ *
+ * These safelists help purgecss know which classes to keep in the final css
+ *
+ *
+ * @example
+ * ```ts
+ * import { defineConfig } from "vite";
+ * import { purgecss, safeLists } from "@shreyas/vite-plugin-purgecss";
+ *
+ * export default defineConfig({
+ *   plugins: [
+ *     purgecss({
+ *			 // include svelte safelist to not purge svelte classes
+ *       safelist: safeLists.svelte,
+ *     }),
+ *   ],
+ * });
+ * ```
+ */
 export const safeLists: Record<string, Options["safelist"]> = {
   svelte: {
     greedy: [SVELTE_SAFELIST],
