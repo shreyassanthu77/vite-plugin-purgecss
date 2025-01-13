c1c2a48418e5edd8f854a03c1717a79643896c01
Shreyas Mididoddi <me@shreyascodes.tech>
2025-01-13 19:12:48 +0530

    code

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-export function hello() {
-  console.log("Hello World!");
+import { PurgeCSS, type Options } from "purgecss";
+import type { Plugin } from "vite";
+
+const CSS_REGEX = /\.css$/;
+
+export function purgecss(options: Options): Plugin {
+  return {
+    name: "@shreyas/vite-plugin-purgecss",
+    enforce: "post",
+    async generateBundle(_, bundle) {
+      const cssFiles: string[] = [];
+      for (const file of Object.keys(bundle)) {
+        if (file.match(CSS_REGEX)) {
+          cssFiles.push(file);
+        }
+      }
+      if (cssFiles.length === 0) return;
+
+      const purgecss = new PurgeCSS();
+      for (const cssFile of cssFiles) {
+        if ("source" in bundle[cssFile]) {
+          const input = bundle[cssFile].source;
+          if (typeof input !== "string") continue;
+          const purged = await purgecss.purge({
+            ...options,
+            content: [{ raw: input, extension: "css" }],
+          });
+          bundle[cssFile].source = purged[0].css;
+        }
+      }
+    },
+  };
 }
+
+export default purgecss;
